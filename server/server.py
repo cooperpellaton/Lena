288047d185be05acafac1bf98b984701759faa5d
Cooper Pellaton <c@cepp.at>
2017-01-21 10:20:57 -0500

    So much has changed, I an't keep track. I also haven't slept so that might be contributing.

--- server/server.py
+++ server/server.py
@@ -7,19 +7,24 @@ import tempfile
 import urllib
 import twitter
 import config
+import json as js
+import tweepy
+from alchemyapi import AlchemyAPI
 from datetime import datetime
 from threading import Thread
 from urllib.request import urlopen
 
-import httplib2
 import numpy as np
-import oauth2client
 import requests
 from oauth2client.client import OAuth2WebServerFlow, Storage
 
-from flask import Flask, Response, jsonify, redirect, request, url_for
+from flask import Flask, Response, jsonify, redirect, request, url_for, send_from_directory
 from flask_login import *
 from pymongo import MongoClient
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/images'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png', 'tif'])
 
 # Setup Twitter API credentials.
 twit = twitter.Api(
@@ -28,11 +33,13 @@ twit = twitter.Api(
     access_token_key=config.access_token_key,
     access_token_secret=config.access_token_secret)
 
-# Single user auth credentials
-http = httplib2.Http()
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token_key, config.access_token_secret)
+api = tweepy.API(auth)
 
 # Flask configuration
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Setup logging
 logging.basicConfig(filename='debug.log', level=logging.INFO)
@@ -47,29 +54,162 @@ threads = []
 @app.route('/api/gallery', methods=["POST"])
 def filter_gallery():
     content = request.get_json()
-    logging.info(json)
-    tags = content['tags']
+    tags = js.loads(content['tags'])
     query = content['query']
-    nltk_filter(tags, query)
+    result = nltk(tags, query)
+    resp = Response(
+        response=js.dumps(result), status=200, mimetype="application/json")
+    return resp
 
 
 @app.route('/api/tag_upload', methods=["POST"])
-def recieve_tags():
-    if(request.data != None):
-        tags = request.get_json()
-        summed_twitter_data = get_twitter_data(tags['tags'])
-        # db.tags.insert_one(tags)
-        resp = Response(
-            response=json.dumps(summed_twitter_data), status=200,  mimetype="text/plain")
-        return resp
+def getHashTags():
+    content = request.get_json()
+    tags = js.loads(content['tags'])
+    logging.info(tags)
+    i = 0
+    tweets = []
+    for tag in tags:
+        if i < 5:
+            max_tweets = 10
+            tweets += [status.text for status in tweepy.Cursor(
+                api.search, q=tag, languages=["en"]).items(max_tweets).filer(safe)]
+        i += 1
+
+    hashtags = {}
+    for tweet in tweets:
+        sentiment = getSentiment(tweet)
+        if sentiment == -2:
+            continue
+        words = tweet.split(" ")
+        for word in words:
+            if len(word) > 2 and word[0] == "#":
+                nw = word[1:].lower()
+                if nw in hashtags:
+                    hashtags[nw]['freq'] += 1
+                    hashtags[nw]['score'] += sentiment
+                else:
+                    hashtags[nw] = {'freq': 1, 'score': sentiment}
+
+    candid = {}
+    for j in hashtags:
+        score = hashtags[j]['score']
+        freq = hashtags[j]['freq']
+        candid[j] = freq*score
+
+    forReturn = sorted(candid, reverse=True)
+    logging.info(forReturn)
+    resp = Response(
+        response=js.dumps(forReturn), status=200, mimetype="application/json")
+    return resp
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+
+def nltk(tags, query):
+    indices = []
+    i = 0
+    for pic in tags:
+        j = 0
+        for tag in pic:
+            if isSimilar(query, tag):
+                indices.append(i)
+                break
+            if j > 2:
+                break
+            j += 1
+        i += 1
+    logging.info(indices)
+    return indices
+
+
+def getSentiment(text):
+    alchemyapi = AlchemyAPI()
+    response = alchemyapi.sentiment('html', text)
+    if response['status'] == 'OK':
+        if 'score' in response['docSentiment']:
+            return float(response['docSentiment']['score'])
+        else:
+            return 0
     else:
-        resp = Response(
-            response="There was a failure with tag upload", status=200,  mimetype="text/plain")
+        return -2
 
 
-def nltk_filter(tags, query):
-        # DO SOME NLTK SHIT HERE.
-    pass
+def isSimilar(word1, word2):
+    logging.info(word1)
+    logging.info(word2)
+    if len(word1) == 0 or len(word2) == 0:
+        return False
+
+    try:
+        with open('words.json') as json_data:
+            d = js.load(json_data)
+    except:
+        d = {}
+    key = word1+word2
+    if word1[0] > word2[0]:
+        key = word2+word1
+
+    if key in d:
+        return d[key]
+
+    url = "https://wordsapiv1.p.mashape.com/words/"+word1
+    auth_headers = {
+        "X-Mashape-Key": "zFpocNrlhomsh7A5WFL8yd3UX8dBp1amWRljsnqrwkVW8V0Udm",
+        "Accept": "application/json"
+    }
+    try:
+        response = requests.get(url, headers=auth_headers)
+        json = js.loads(response.text)
+
+    except:
+        json = ""
+
+    logging.info(json)
+    d[key] = (word2 in js.dumps(json['results']))
+
+    with open('words.json', 'w') as outfile:
+        js.dump(d, outfile)
+
+    return d[key]
 
 
 def get_twitter_data(tags):
@@ -79,9 +219,10 @@ def get_twitter_data(tags):
         for x in tags:
             for y in x:
                 string = string + "%20" + y + "OR"
-            string = string + "%20include%3Aretweets"
-            results = api.GetSearch(string)
-            twitter_data.append(results)
+            string = string + "%20include%3Aretweets&count=100"
+            results = twit.GetSearch(raw_query=string)
+            data = json.dumps(results)
+            twitter_data.append(data)
         return twitter_data
     except:
         logging.info(
